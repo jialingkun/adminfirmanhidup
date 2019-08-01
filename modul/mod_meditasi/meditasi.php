f3eb52d2498200dac03dcbc9a3a6b17142d1f469
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2019-08-01 20:50:49 +0700

    Update meditasi.php

--- modul/mod_meditasi/meditasi.php
+++ modul/mod_meditasi/meditasi.php
@@ -6,282 +6,288 @@ include "config/koneksi.php";
 
 <script type="text/javascript" src="modul/mod_meditasi/aksi_meditasi.js"></script>
 
-                                
-                            <div class="panel panel-bordered border-danger">
-                                
-                                <div class="panel-body">
+
+<div class="panel panel-bordered border-danger">
+
+    <div class="panel-body">
+
+        <div class="col-md-12">
+
+            <div class="col-md-2">
+                <button type="button" onclick="tambah_data(this);" class="btn bg-grey-800 btn-block"><i class="icon-add"></i> Tambah Data</button>
+                <br>
+            </div>
+            
+            <!-- embed -->
+            <div class="col-md-10">
+                <div class="col-md-3"><b style="float:right;">Embed</b></div>
+                <div class="col-md-8"><input type="text" style="width:100%;" placeholder="embed text" value=""></div>
+                <div class="col-md-1"><button type="button" class="btn btn-success" onclick="">SAVE</button></div>
+            </div>
+        </div>
+
+
+        <div class="col-md-3" style="display: none;">
+
+            <div class="form-group">
+                <div class="input-group">
+
+                    <span class="input-group-addon"><i class="icon-menu-open"></i></span>
+
+                    <select class="boostrap-select" id="pilihan_cari">
+                      <option value="SEMUA">SEMUA</option>
+                      <option value="EMAIL">EMAIL</option>
+                      <option value="NAMA">NAMA</option>
+
+
+                  </select>
+              </div>
+          </div> 
+      </div>
+
+
+
+      <div class="col-md-6">
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="icon-search4"></i></span>
+                <input type="text" id="cari_meditasi" class="form-control" placeholder="Cari Meditasi . . .">
+            </div>
+        </div> 
+    </div>
+
+
+
+
+
+    <div class="col-md-3">
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="icon-menu-open"></i></span>
+
+                <select class="boostrap-select" id="batas_hal">
+                  <option value="">Tampilkan : 100 Data</option>
+                  <option value="">Tampilkan : 50 Data</option>
+                  <option value="">Tampilkan : 10 Data</option>
+              </select>
+          </div>
+      </div> 
+  </div>
+
+
+
+  <div class="col-md-3">
+    <div id="halaman">
+
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="icon-menu-open"></i></span>
+
+                <select class="boostrap-select">
+                  <option value="1">Halaman : 1</option>
+              </select>
+          </div>
+
+
+      </div>
+
+
+  </div>
+</div>
+
+
+
 
 <div class="col-md-12">
-
-								<div class="col-md-3">
-                                    <button type="button" class="btn bg-grey-800 btn-block" onClick="tambah_data(this);"><i class="icon-add"></i> Tambah Data Baru</button>
-
-<br>
+  <div class="table-responsive">
+    <table id="tabel_meditasi" class="table table-hover table-bordered table-xxs">
+        <thead>
+            <tr class="bg-grey-800">
+                <th class="col-xs-1 text-center">NO</th>
+                <th class="text-center col-xs-1><a href="javascript:;" id='tanggal' class="text-white" onclick="order_by(this);">TANGGAL &nbsp; <i class="icon-menu-open"></i></a></th>
+                <th class="text-center col-xs-1">JAM</th>
+                <th>MATERI MEDITASI</th>
+                <th class="text-center col-xs-1">INTERVAL</th>
+                <th class="text-center col-xs-1" style="display: none;">ID</th>
+            </tr>
+        </thead>
+    </table>
 </div>
+
+
+
+
+<div class="context-table">
+    <ul class="dropdown-menu border-grey">
+
+
+
+
+        <li><a href="#"><i class="icon-add"></i> Tambah Data</a></li>
+
+
+        <li class="divider"></li>
+
+        <li><a href="#"><i class="icon-pencil3"></i> Edit Data</a></li>
+        <li><a href="#"><i class="icon-bin"></i> Hapus Data</a></li>
+
+        <li class="divider"></li>
+
+        <li><a href="#"><i class="icon-database-refresh"></i> Segarkan Data</a></li>
+
+
+
+    </ul>
 </div>
 
 
-<div class="col-md-3" style="display: none;">
 
-<div class="form-group">
-    <div class="input-group">
-
-        <span class="input-group-addon"><i class="icon-menu-open"></i></span>
-
-    <select class="boostrap-select" id="pilihan_cari">
-      <option value="SEMUA">SEMUA</option>
-      <option value="EMAIL">EMAIL</option>
-      <option value="NAMA">NAMA</option>
+</div>
 
 
-    </select>
+
+
+
+</div>
+
+
+</div>
+
+
+<div id="form-tambah" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-grey-800">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h6 class="modal-title"><i class="icon-add"></i> &nbsp; Tambah Data</h6>
+            </div>
+
+            <div class="modal-body">
+
+
+
+                <div class="col-md-12">
+                    <div class="form-group">
+                       <label class="text-grey-800"><b>Interval Program (Menit)</b></label>
+
+                       <div class="input-group">
+                        <span class="input-group-addon"><i class="icon-pencil3"></i></span>
+                        <input type="text" id="interval_program" class="form-control text-right" placeholder="Masukkan Interval Program (Menit)">
+                    </div>
+                </div> 
+            </div>
+
+
+
+            <div class="col-md-12">
+                <div class="form-group">
+                   <label class="text-grey-800"><b>Materi Meditasi</b></label>
+
+                   <textarea rows="5" cols="5" id="meditasi" class="form-control" placeholder="Masukkan Materi Meditasi"></textarea>
+               </div> 
+           </div>
+
+
+
+
+       </div>
+
+       <div class="modal-footer">
+        <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
     </div>
-</div> 
+</div>
+</div>
 </div>
 
-                                        
-
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="icon-search4"></i></span>
-                                            <input type="text" id="cari_meditasi" class="form-control" placeholder="Cari Meditasi . . .">
-                                        </div>
-                                        </div> 
-                                        </div>
 
 
 
 
 
-<div class="col-md-3">
-<div class="form-group">
-    <div class="input-group">
-            <span class="input-group-addon"><i class="icon-menu-open"></i></span>
+<!-- Danger modal -->
+<div id="form-edit" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-grey-800">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h6 class="modal-title"><i class="icon-pencil7"></i> &nbsp; Edit Data</h6>
+            </div>
 
-    <select class="boostrap-select" id="batas_hal">
-      <option value="">Tampilkan : 100 Data</option>
-      <option value="">Tampilkan : 50 Data</option>
-      <option value="">Tampilkan : 10 Data</option>
-    </select>
+            <div class="modal-body">
+                <input type="hidden" id="id" class="form-control">
+
+
+                <div class="col-md-12">
+                    <div class="form-group">
+                       <label class="text-grey-800"><b>Interval Program (Menit)</b></label>
+
+                       <div class="input-group">
+                        <span class="input-group-addon"><i class="icon-pencil3"></i></span>
+                        <input type="text" id="interval_program" class="form-control text-right" placeholder="Masukkan Interval Program (Menit)">
+                    </div>
+                </div> 
+            </div>
+
+
+
+            <div class="col-md-12">
+                <div class="form-group">
+                   <label class="text-grey-800"><b>Materi Meditasi</b></label>
+
+                   <textarea rows="5" cols="5" id="meditasi" class="form-control" placeholder="Masukkan Materi Meditasi"></textarea>
+               </div> 
+           </div>
+
+
+
+
+
+       </div>
+
+       <div class="modal-footer">
+        <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
     </div>
-</div> 
+</div>
+</div>
 </div>
 
 
 
-<div class="col-md-3">
-<div id="halaman">
-    
-<div class="form-group">
-    <div class="input-group">
-            <span class="input-group-addon"><i class="icon-menu-open"></i></span>
+<div id="form-upload" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-grey-800">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h6 class="modal-title"><i class="icon-add"></i> &nbsp; Upload Foto</h6>
+            </div>
 
-    <select class="boostrap-select">
-      <option value="1">Halaman : 1</option>
-    </select>
+            <div class="modal-body">
+
+
+
+                <input type="hidden" id="nik">
+
+
+
+                <div class="col-md-12">
+
+                    <div align="center">
+                        <input type="image" id="image" value="" class="img-responsive" width="200">
+                    </div>
+                    <input type="file" id="my_file" style="display: none;" />
+
+
+
+                </div>
+
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
+            </div>
+        </div>
     </div>
-
-
 </div>
-
-
-</div>
-</div>
-
-
-
-
-<div class="col-md-12">
-      <div class="table-responsive">
-            <table id="tabel_meditasi" class="table table-hover table-bordered table-xxs">
-                                <thead>
-                                    <tr class="bg-grey-800">
-                                    <th class="col-xs-1 text-center">NO</th>
-                                    <th class="text-center col-xs-1><a href="javascript:;" id='tanggal' class="text-white" onclick="order_by(this);">TANGGAL &nbsp; <i class="icon-menu-open"></i></a></th>
-                                    <th class="text-center col-xs-1">JAM</th>
-                                    <th>MATERI MEDITASI</th>
-                                    <th class="text-center col-xs-1">INTERVAL</th>
-                                    <th class="text-center col-xs-1" style="display: none;">ID</th>
-                                    </tr>
-                                </thead>
-                            </table>
-                    </div>
-
-
-
-
-  <div class="context-table">
-                                    <ul class="dropdown-menu border-grey">
-
-
-                                   
-
-                                        <li><a href="#"><i class="icon-add"></i> Tambah Data</a></li>
-
-
-                                        <li class="divider"></li>
-                                 
-                                        <li><a href="#"><i class="icon-pencil3"></i> Edit Data</a></li>
-                                        <li><a href="#"><i class="icon-bin"></i> Hapus Data</a></li>
-
-                                        <li class="divider"></li>
-
-                                        <li><a href="#"><i class="icon-database-refresh"></i> Segarkan Data</a></li>
-                                     
-                                     
-
-                                    </ul>
-                                </div>
-
-
-
-                                </div>
-
-
-
-
-
-                            </div>
-                        
-
-</div>
-
-
-                    <div id="form-tambah" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header bg-grey-800">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <h6 class="modal-title"><i class="icon-add"></i> &nbsp; Tambah Data</h6>
-                                </div>
-
-                                <div class="modal-body">
-
-
-
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                       <label class="text-grey-800"><b>Interval Program (Menit)</b></label>
-                                       
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="icon-pencil3"></i></span>
-                                            <input type="text" id="interval_program" class="form-control text-right" placeholder="Masukkan Interval Program (Menit)">
-                                        </div>
-                                        </div> 
-                                        </div>
-
-
-
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                       <label class="text-grey-800"><b>Materi Meditasi</b></label>
-                                       
-                                            <textarea rows="5" cols="5" id="meditasi" class="form-control" placeholder="Masukkan Materi Meditasi"></textarea>
-                                        </div> 
-                                        </div>
-
-                                        
-
-
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-
-
-
-                    <!-- Danger modal -->
-                    <div id="form-edit" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header bg-grey-800">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <h6 class="modal-title"><i class="icon-pencil7"></i> &nbsp; Edit Data</h6>
-                                </div>
-
-                                <div class="modal-body">
-                    <input type="hidden" id="id" class="form-control">
-                    
-
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                       <label class="text-grey-800"><b>Interval Program (Menit)</b></label>
-                                       
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="icon-pencil3"></i></span>
-                                            <input type="text" id="interval_program" class="form-control text-right" placeholder="Masukkan Interval Program (Menit)">
-                                        </div>
-                                        </div> 
-                                        </div>
-
-
-
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                       <label class="text-grey-800"><b>Materi Meditasi</b></label>
-                                       
-                                            <textarea rows="5" cols="5" id="meditasi" class="form-control" placeholder="Masukkan Materi Meditasi"></textarea>
-                                        </div> 
-                                        </div>
-
-                                        
-
-
-
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                    <div id="form-upload" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header bg-grey-800">
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <h6 class="modal-title"><i class="icon-add"></i> &nbsp; Upload Foto</h6>
-                                </div>
-
-                                <div class="modal-body">
-
-
-
-                                            <input type="hidden" id="nik">
-                                   
-
-								   
-                                        <div class="col-md-12">
-
-<div align="center">
-<input type="image" id="image" value="" class="img-responsive" width="200">
-</div>
-                            <input type="file" id="my_file" style="display: none;" />
-
-
-
-</div>
-
-
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="button" class="btn bg-grey-800 btn-block" id="simpan"><i class="icon-database-check"></i> Simpan</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
 
